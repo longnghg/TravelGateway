3385848e0eec14b89267e561aacf9c15539d769f
H1ieenrrr <hienltps16180@fpt.edu.vn>
2022-12-24 17:20:58 +0700

    init

---
+++ APIUser/APIUser/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using APIUser.Database;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace APIUser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        ContextDb db;
+        public UserController()
+        {
+            db = new ContextDb();
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return db.Users.ToList();
+        }
+
+     
+        // POST api/<UserController>
+        [HttpPost]
+        public User Post(User user)
+        {
+            return user;
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut]
+        [Route("Update/Car")]
+        public void Put(User us,int id)
+        {
+            var LD = id;
+            var v = 123;
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete]
+        [Route("delete/car")]
+        public void Delete(int id)
+        {
+            var LD = id;
+            var v = 123;
+        }
+    }
+}
---
+++ APIUser/Gate1/TravelHub.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gate1
+{
+    [Authorize]
+
+    public class TravelHub: Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            if (httpContext != null)
+            {
+                var jwtToken = httpContext.Request.Query["access_token"];
+
+                var handler = new JwtSecurityTokenHandler();
+                if (!string.IsNullOrEmpty(jwtToken))
+                {
+                    var token = handler.ReadJwtToken(jwtToken);
+                    var tokenS = token as JwtSecurityToken;
+                    var jti = tokenS.Claims.Where(x => x.Type == "RoleId").FirstOrDefault();
+
+                    // replace email with your claim name
+
+                    if (jti != null && jti.Value != "")
+                    {
+                        Groups.AddToGroupAsync(Context.ConnectionId, jti.Value.ToUpper());
+                    }
+                }
+            }
+            return base.OnConnectedAsync();
+        }
+        public async Task Block(string idUser)
+        {
+            await Clients.User(idUser.ToUpper()).SendAsync("BlockUser");
+
+        }
+        public async Task SendNotyf(string idRole)
+        {
+            var arrIdRole = idRole.Split(',');
+            foreach (var id in arrIdRole)
+            {
+                await Clients.Group(id.ToUpper()).SendAsync("Notification");
+            }
+        }
+        public async Task Chat(string idUser)
+        {
+            await Clients.User(idUser.ToUpper()).SendAsync("Message");
+            //await Clients.Group("b76c4137-c497-42f5-821d-554a51862e45").SendAsync("Init");
+        }
+        public override async Task OnDisconnectedAsync(Exception ex)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, Context.User.Identity.Name);
+            await base.OnDisconnectedAsync(ex);
+        }
+        public async Task Send()
+        {
+            var httpContext = Context.GetHttpContext();
+            await Clients.All.SendAsync("Init");
+        }
+    }
+}
